3eb4e74153672cf90370022ea1f093d3ff728c26
Juan Garcia <juangalbea@gmail.com>
2019-12-08 22:06:54 +0100

    array-syntax

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    toggleClass: false,
+    firstOne: 'firstClass',
+    secondOne: 'secondClass'
   },
   methods: {
     startEffect: function () {
       var vm = this;
       setInterval(function () {
-
+        vm.toggleClass = !vm.toggleClass;
       }, 2000)
     }
   }
